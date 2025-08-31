6d53a92498c2ac6c7926f1bb6fa1534a394ba52f
Gwen Buchthal <gwenbuchthal@microsoft.com>
2025-08-31 15:39:23 -0700

    scaffold DI container

---
+++ OrderIngest/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace OrderIngest
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            // Load configuration from JSON files
+            builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                 .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                                 .AddUserSecrets<Program>();
+
+            IHost host = builder.Build();
+        }
+    }
+}
