6f355a25632b0fabc98427c9f937cf8055a58943
Gwen Buchthal <gwenbuchthal@microsoft.com>
2025-08-31 15:40:54 -0700

    create custom logger

---
+++ OrderIngest/Services/Common/IOrderLogger.cs
@@ -0,0 +1,42 @@
+﻿namespace OrderIngest.Services.Common;
+
+using System;
+
+public interface IOrderLogger
+{
+    /// <summary>
+    /// Logs a trace-level message.
+    /// </summary>
+    /// <param name="category">The category indicates what the application was doing when the log fired.</param>
+    /// <param name="message">The message to log.</param>
+    void LogTrace(LogCategory category, string message);
+
+    /// <summary>
+    /// Logs a debug-level message.
+    /// </summary>
+    /// <param name="category">The category indicates what the application was doing when the log fired.</param>
+    /// <param name="message">The message to log.</param>
+    void LogDebug(LogCategory category, string message);
+
+    /// <summary>
+    /// Logs an information-level message.
+    /// </summary>
+    /// <param name="category">The category indicates what the application was doing when the log fired.</param>
+    /// <param name="message">The message to log.</param>
+    void LogInformation(LogCategory category, string message);
+
+    /// <summary>
+    /// Logs a warning-level message.
+    /// </summary>
+    /// <param name="category">The category indicates what the application was doing when the log fired.</param>
+    /// <param name="message">The message to log.</param>
+    void LogWarning(LogCategory category, string message);
+
+    /// <summary>
+    /// Logs an error-level message.
+    /// </summary>
+    /// <param name="category">The category indicates what the application was doing when the log fired.</param>
+    /// <param name="message">The message to log.</param>
+    /// <param name="ex">the exception to log (if any)</param>
+    void LogError(LogCategory category, string message, Exception? ex);
+}
---
+++ OrderIngest/Services/Common/OrderLogger.cs
@@ -0,0 +1,72 @@
+﻿namespace OrderIngest.Services.Common;
+
+using Microsoft.Extensions.Logging;
+using System;
+
+public class OrderLogger : IOrderLogger
+{
+    /// <summary>
+    /// The base logger service instance.
+    /// </summary>
+    private readonly ILogger<OrderLogger> _logger;
+
+    /// <summary>
+    /// Creates a new <see cref="IOrderLogger"/> instance.
+    /// </summary>
+    /// <param name="logger"></param>
+    public OrderLogger(ILogger<OrderLogger> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <inheritdoc/>
+    public void LogTrace(LogCategory category, string message)
+    {
+        _logger.LogTrace("[{Category}] {Message}", category, message);
+    }
+
+    /// <inheritdoc/>
+    public void LogDebug(LogCategory category, string message)
+    {
+        _logger.LogDebug("[{Category}] {Message}", category, message);
+    }
+
+    /// <inheritdoc/>
+    public void LogInformation(LogCategory category, string message)
+    {
+        _logger.LogInformation("[{Category}] {Message}", category, message);
+    }
+
+    /// <inheritdoc/>
+    public void LogWarning(LogCategory category, string message)
+    {
+        _logger.LogWarning("[{Category}] {Message}", category, message);
+    }
+
+    /// <inheritdoc/>
+    public void LogError(LogCategory category, string message, Exception? ex)
+    {
+        _logger.LogError(ex, "[{Category}] {Message}", category, message);
+    }
+}
+
+/// <summary>
+/// The category of the log message, associated with the various application features.
+/// </summary>
+public enum LogCategory
+{
+    /// <summary>
+    /// Log messages related to application startup.
+    /// </summary>
+    Startup,
+
+    /// <summary>
+    /// Log messages related to ingesting the raw order message.
+    /// </summary>
+    Ingest,
+
+    /// <summary>
+    /// Log messages related to publishing the structured order message.
+    /// </summary>
+    Publish,
+}
